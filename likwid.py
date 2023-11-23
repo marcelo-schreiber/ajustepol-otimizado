d416f7c5a44b2189188c5fc4613ab0fee093ca30
marcelo-schreiber <marcelorissette15@gmail.com>
2023-11-22 22:39:47 -0300

    Refactor likwid.py and update sample_inputs

--- likwid.py
+++ likwid.py
@@ -25,11 +25,12 @@ likwid_metrics = {
 
 likwid_markers = ["generate_matrix", "solve_system", "residue"]
 
-sample_inputs = [64, 128, 200, 256, 512, 600, 800, 1024, 2000, 3000, 4096, 6000, 7000, 10000, 50000, 100_000, 1_000_000, 10_000_000, 100_000_000]  
+sample_inputs = [64, 128]#, 200, 256, 512, 600, 800, 1024, 2000, 3000, 4096, 6000, 7000, 10000, 50000, 100_000, 1_000_000, 10_000_000, 100_000_000]  
 
 system(f"echo \"performance\" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor")    
 system("gcc gera_entrada.c -o gera_entrada")
 system("make")
+system("mkdir results")
 
 def get_metric_number_from_file(filename, metric):
     tracked_metrics = {}
@@ -78,14 +79,14 @@ for i in sample_inputs:
         tracked_metrics = get_metric_number_from_file(f"{metric}_tmp.txt", likwid_metrics[metric]['label'])
 
         for marker in tracked_metrics:
-            with open(f"{metric}_{marker}.txt", 'a') as f:
+            with open(f"./results/{metric}_{marker}.txt", 'a') as f:
                 f.write(f"{i} {tracked_metrics[marker]}\\\\\n")
   
 
     runtimes = get_runtime_from_file(f"{metric}_tmp.txt")
-    system(f"rm {metric}_tmp.txt")
+
     for marker in runtimes:
-        with open(f"runtime_{marker}.txt", 'a') as f:
+        with open(f"./results/runtime_{marker}.txt", 'a') as f:
             f.write(f"{i} {runtimes[marker]}\\\\\n")
 
 
